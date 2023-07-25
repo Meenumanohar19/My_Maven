71a9bccea79ea5e8134903c71077ddbc90a6fe2b
aniru <aniru@DESKTOP-GGRBDL6>
2023-07-25 08:49:58 +0530

    My 3rd commit on 25/07/2023

---
+++ My_MavenNew/src/main/java/newsample/Example.java
@@ -0,0 +1,34 @@
+package newsample;
+
+public class Example {
+
+	int age; //instance variable
+    int mark;   //instance variable
+	
+   
+	public Example(int age,int mark)
+	{
+		this.age=age;
+		this.mark=mark;	
+		System.out.println(age);
+		System.out.println(mark);
+		this.add(3.5f,10.9d);
+	}
+	public void add(float f,double d)
+	{
+		this.sub();
+		System.out.println(f+d);
+	}
+	public void sub()
+	{
+		int x=4;
+		int y=5;
+		System.out.println(x-y);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Example obj=new Example(25,50);
+	}
+
+}
